e8de2bd190cb878aa96704304b7afd6cb129ae12
Norman Joyner <norman.joyner@gmail.com>
2017-01-10 21:04:48 -0500

    move avd creation

--- scripts/docker/run-ci-e2e-tests.sh
+++ scripts/docker/run-ci-e2e-tests.sh
@@ -69,8 +69,14 @@ fi
 
 if [ $RUN_ANDROID -ne 0 ]; then
     set +ex
+
+    AVD_UUID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
+
+    # create virtual device
+    RUN echo no | android create avd -n $AVD_UUID -f -t android-19 --abi default/armeabi-v7a
+
     # emulator setup
-    emulator64-arm -avd testAVD -no-skin -no-audio -no-window -no-boot-anim &
+    emulator64-arm -avd $AVD_UUID -no-skin -no-audio -no-window -no-boot-anim &
     bootanim=""
     until [[ "$bootanim" =~ "stopped" ]]; do
         sleep 5
